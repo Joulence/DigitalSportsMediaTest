c619d488be4e7d11c9a74149d6dcba27cbf6cac0
Joulence <4erviken@gmail.com>
2023-11-15 23:18:31 +0200

    Updated main js file

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 // import * as isWebP from "./modules/isWeb.js";
 import * as menu from "./modules/menu.js";
-
+import * as chooseDay from "./modules/chooseForecastDay.js";
 
 // isWebP.isWebP();
 menu.onclick();
+chooseDay.chooseDay();
