153174250d246e006fd27b194c416fdd1d67e8fd
Joulence <4erviken@gmail.com>
2023-11-17 13:26:55 +0200

    added logic for match days buttons

--- src/js/modules/chooseForecastDay.js
+++ src/js/modules/chooseForecastDay.js
@@ -2,6 +2,29 @@ export function chooseDay() {
   const forecastDays = document.querySelector(".forecasts__inner-forecast")
     .children[1];
 
+  const matchDays = document.querySelector(".match-buttons");
+
+  matchDays.addEventListener("click", (event) => {
+    if (
+      event.target.tagName === "BUTTON" &&
+      !event.target.classList.contains("match-calendar")
+    ) {
+      if (!event.target.classList.contains("selected-sport")) {
+        for (let i = 0; i < matchDays.children.length; i++) {
+          matchDays.children[i].classList.remove("selected-sport");
+        }
+
+        for (let i = 0; i < matchDays.children[1].children.length; i++) {
+          // console.log(matchDays.children[1].children[i])
+          matchDays.children[1].children[i].children[0].classList.remove(
+            "selected-sport"
+          );
+        }
+        event.target.classList.add("selected-sport");
+      }
+    }
+  });
+
   forecastDays.addEventListener("click", (event) => {
     if (event.target.tagName === "BUTTON") {
       if (!event.target.classList.contains("selected")) {
