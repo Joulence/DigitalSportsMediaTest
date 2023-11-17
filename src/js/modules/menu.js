bc914415051bf622b2d2cb9d79d36e3b7e8bea2b
Joulence <4erviken@gmail.com>
2023-11-17 16:58:26 +0200

    added close menu by clicking on backdrop

--- src/js/modules/menu.js
+++ src/js/modules/menu.js
@@ -3,13 +3,32 @@ export function onclick() {
   let closeMenuBtn = document.getElementById("close-menu");
   let navMenu = document.getElementById("nav-sidebar");
 
-  openMenuBtn.addEventListener("click", () => {
+  const addSideBar = () => {
     navMenu.classList.toggle("show");
     document.querySelector("header").classList.toggle("menu-open");
-  });
+    document.querySelector("main").classList.toggle("menu-open");
+    document.body.classList.add("overflow-hidden");
+  };
 
-  closeMenuBtn.addEventListener("click", () => {
+  const removeSideBar = () => {
     navMenu.classList.toggle("show");
     document.querySelector("header").classList.toggle("menu-open");
+    document.querySelector("main").classList.toggle("menu-open");
+    document.body.classList.remove("overflow-hidden");
+  };
+
+  openMenuBtn.addEventListener("click", () => addSideBar());
+
+  closeMenuBtn.addEventListener("click", () => removeSideBar());
+
+  document.body.addEventListener("click", (event) => {
+    if (navMenu.classList.contains("show")) {
+      if (
+        event.target.tagName === "HEADER" ||
+        event.target.tagName === "MAIN"
+      ) {
+        removeSideBar();
+      }
+    }
   });
 }
